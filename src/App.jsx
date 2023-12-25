9101e4d789dc7db2019451c1f9fc7b0daeaa5448
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-12-25 12:29:53 -0300

    Main and component update additing style

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import HeaderGame from "./components/HeaderGame";
+
 export default () => {
     return (
         <>
+            <HeaderGame />
             <section className="pt-5">
-                <div className="container text-center bg-secondary rounded mt-5 p-4">
-                    <h1>Animal fight</h1>
-                    <div className="d-grid gap-2 col-6 mx-auto">
-                        <button className="btn btn-primary">jogar</button>
-                        <button className="btn btn-primary" disabled>
+                <div className="container text-center bg-img-back rounded mt-5 p-5">
+                    <h1 className="title-principal display-5 pb-4">
+                        Animal fight
+                    </h1>
+                    <div className="d-grid gap-2 col-8 mx-auto">
+                        <button className="btn btn-lg btn-game">jogar</button>
+                        <button className="btn btn-lg btn-game" disabled>
                             Opções
                         </button>
                     </div>
