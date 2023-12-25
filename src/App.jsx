bf3d0a3b90e1d020b8f7192e1dbe007bc326fe08
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-12-25 11:04:06 -0300

    Update main

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 export default () => {
     return (
         <>
-            <h1>init game</h1>
+            <section className="pt-5">
+                <div className="container text-center bg-secondary rounded mt-5 p-4">
+                    <h1>Animal fight</h1>
+                    <div className="d-grid gap-2 col-6 mx-auto">
+                        <button className="btn btn-primary">jogar</button>
+                        <button className="btn btn-primary" disabled>
+                            Opções
+                        </button>
+                    </div>
+                </div>
+            </section>
         </>
     );
 };
